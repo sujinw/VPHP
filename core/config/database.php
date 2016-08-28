21aad1f1b3c0751b1b185a4d236c960a0401731a
Salde <sujinw@qq.com>
2016-08-28 13:45:23 +0800

    vphp first

---
+++ core/config/database.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    // 必须配置项
+    'database_type' => 'mysql',
+    'database_name' => 'world',
+    'server' => 'localhost',
+    'username' => 'root',
+    'password' => 'smn123',
+    'charset' => 'utf8',
+);
